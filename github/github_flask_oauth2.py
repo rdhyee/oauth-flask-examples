3ef3191bc1a6f2a2e3480610dbd258d1c6849f6b
Raymond Yee <raymond.yee@gmail.com>
2015-11-09 18:01:17 -0800

    the github Dockerfile works well now -- I can go to https://github-oauth.ydns.eu and the requests get sent to the right container as I long as I have the nginx-proxy container running

--- github/github_flask_oauth2.py
+++ github/github_flask_oauth2.py
@@ -79,3 +79,4 @@ if __name__ == "__main__":
 
     app.secret_key = os.urandom(24)
     app.run(host="0.0.0.0", debug=True, port=5000)
+    #app.run(debug=True, port=5000)
--- github/run_docker.sh
+++ github/run_docker.sh
@@ -1,10 +1,19 @@
 set -e
 docker build -t rdhyee/github-oauth .
-PORT=${1:-80}
+
+# create server.key, server.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt -subj "/C=XX/ST=XX/L=XX/O=dockergenerated/CN=dockergenerated"
+
+# copy the certs
+docker cp server.crt certs:/etc/nginx/certs/github-oauth.ydns.eu.crt
+docker cp server.key certs:/etc/nginx/certs/github-oauth.ydns.eu.key
 
 # jupyter stack images (don't map local drive)
-docker run -d --name github -p $PORT:5000 \
+docker run -d --name github  \
+   -e VIRTUAL_HOST=github-oauth.ydns.eu \
+   -e VIRTUAL_PORT=5000 \
    -e GITHUB_CONSUMER_KEY=$GITHUB_CONSUMER_KEY \
    -e GITHUB_CONSUMER_SECRET=$GITHUB_CONSUMER_SECRET \
    -e GITHUB_REDIRECT_URI=$GITHUB_REDIRECT_URI \
+   -e OAUTHLIB_INSECURE_TRANSPORT=1 \
      rdhyee/github-oauth
